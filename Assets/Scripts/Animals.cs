410e7ffc9702032194b95da7bba204c040657c9a
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2023-05-19 13:37:43 +0200

    Animación del mono.
    Al pulsar una tecla el mono pasa de quieto a moverse.

--- Assets/Scripts/Animals.cs
+++ Assets/Scripts/Animals.cs
@@ -7,6 +7,7 @@ public class Animals : MonoBehaviour
     private float horizontalInput;
     private float turnSpeed = 120.0f;
     private Vector3 angleRotation = new Vector3(0, 5, 0);
+    protected Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +20,17 @@ public class Animals : MonoBehaviour
         
     }
 
-    protected virtual void Rotation()
+    protected void Rotation()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(angleRotation, Time.deltaTime * turnSpeed * - horizontalInput);
+    }
+
+    protected virtual void StandardAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            playerAnimator.SetBool("", true);
+        }
     }
 }
--- Assets/Scripts/Monkey.cs
+++ Assets/Scripts/Monkey.cs
@@ -7,12 +7,25 @@ public class Monkey : Animals
     // Start is called before the first frame update
     void Start()
     {
-       
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        StandardAnimation();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Rotation();
+    }
+
+    protected override void StandardAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            playerAnimator.SetBool("Idle_a_bool", true);
+        }
     }
 }
